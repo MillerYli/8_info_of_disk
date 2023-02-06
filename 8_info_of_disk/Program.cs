dafaae88455368d8999ffa372e5ba2a8ad0e671b
Yulia Miller <miller82.98@mail.ru>
2023-02-05 21:25:42 -0800

    Добавьте файлы проекта.

---
+++ 8_info_of_disk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Получим системные диски
+             DriveInfo[] drives = DriveInfo.GetDrives();
+
+            //Пройдемся по дискам и выведем их свойста
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if(drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
